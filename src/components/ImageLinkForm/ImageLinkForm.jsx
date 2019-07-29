1a307bb1e800a2a6f8cec1f67f7fad3d4399ac50
Damyan Peykov <dppeykov@gmail.com>
2019-07-29 16:56:25 +0200

    Added the image link form
    still needs work to be done on it

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
+import "./ImageLinkForm.css";
+
 const ImageLinkForm = () => (
   <div>
-    <p className="f4 tc ma4">
+    <p className="f4 tc ma4 mt5">
       {`This magic application will detect the faces in your pictures.`} <br />
       {`Give it a try!`}
     </p>
+    <div className="center">
+      <div className="form center ma2">
+        <input className="f4 pa2 w-70 center br4 br--left" type="text" />
+        <button className="w-30 grow f5 br4 br--right link ph3 pv2 dib white bg-orange pointer">
+          Detect
+        </button>
+      </div>
+    </div>
   </div>
 );
 
