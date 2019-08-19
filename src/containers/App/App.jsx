c73c0e00a773b99966884b3744cb5a01d3631eb3
Damyan Peykov <dppeykov@gmail.com>
2019-08-19 10:49:54 +0200

    Without clarifai installed
    run npm install clarifai

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import Navigation from "../../components/Navigation/Navigation";
 import Rank from "../../components/Rank/Rank"
 import ImageLinkForm from "../../components/ImageLinkForm/ImageLinkForm";
+import Clarifai from 'clarifai';
+
+// run npm install clarifai
+
+
+const app = new Clarifai.App({
+  apiKey: '06d95d6f6ecc41168836bd1818223c47'
+ });
 
 class App extends Component {
   constructor() {
@@ -14,7 +22,16 @@ class App extends Component {
 
   onInputChange = e => this.setState({input: e.target.value})
 
-  onButtonSubmit = () => console.log('click')
+  onButtonSubmit = () => {
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
 
   render() {
     return (
