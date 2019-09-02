06fcbd9bb6717259d7ce290c4a17eab84262b942
Damyan Peykov <dppeykov@gmail.com>
2019-09-02 22:46:58 +0200

    tachyons comments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./containers/App/App";
-
+//css framework for functional css
 import "tachyons";
 import "./index.css";
 
